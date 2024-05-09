9af554375fddd0df035cff8147fdd1e717d6c44a
Anders Johansson <trumanders@gmail.com>
2024-05-09 21:05:34 +0200

    Delete Car and Motorbike

--- TollFeeCalculatorV2/Car.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TollFeeCalculatorV2;
-
-public class Car : Vehicle
-{
-	public Car(string name, VehicleTypes types) : base(name, types) { }
-}
--- TollFeeCalculatorV2/Motorbike.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TollFeeCalculatorV2;
-
-public class Motorbike : Vehicle
-{
-	public Motorbike(string name, VehicleTypes types) : base(name, types) {	}
-}
